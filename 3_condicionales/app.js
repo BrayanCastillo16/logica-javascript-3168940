e7514d4ec9ac672f75fbbc7aee025b638baaed85
BrayanCastillo16 <brayancastillosepulveda16@gmail.com>
2025-08-05 13:28:42 -0500

    3_condicionales

---
+++ 3_condicionales/app.js
@@ -0,0 +1,31 @@
+
+//Ejercicio numero 1
+
+//let nombre = prompt("dame tu nombre");
+//let edad = prompt("dame tu edad");
+
+ //if (edad >= 18) {
+ //alert ("eres mayor de edad puedes ingresar a el restaurante");
+ //}else {
+    //alert ("No puedes ingresar a el restaurante")
+//}
+// if (nombre.toLowerCase()===  "brayan" , nombre.toLowerCase()=== "maria") {
+   // alert("eres cliente VIP Pide lo que quieras");
+//}
+
+//Ejercicio numero 2
+
+let estatura = prompt("ingresa cuanto mides");
+let velocidad = prompt("ingresa tu velocidad");
+let edad = prompt("ingresa tu edad");
+
+if (estatura>=170 && velocidad >= 27 , edad<18 ) {
+alert("cumples con los requisitos para ingresar a el restaurante");
+}
+if (edad<=18){
+alert("felicidades ya llegara tu pedido");
+
+}
+if (edad<=18){
+alert("tu pedido esta en camino");
+ }
